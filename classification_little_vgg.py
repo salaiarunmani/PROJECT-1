0ef836db4058bcc43de593eb0fc7d0d00d16aa1b
salaiarunmani <80448838+salaiarunmani@users.noreply.github.com>
2021-08-10 18:00:26 +0530

    Update classification_little_vgg.py

--- classification_little_vgg.py
+++ classification_little_vgg.py
@@ -3,15 +3,15 @@ import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense,Dropout,Activation,Flatten,BatchNormalization #
-from keras.layers import Conv2D,MaxPooling2D
+from keras.layers import Conv2D,MaxPooling2D #convert2d and maxpooling
 import os
 
-num_classes = 5
-img_rows,img_cols = 48,48
-batch_size = 8 # how many images train at a time
+num_classes = 5 #5 emotion
+img_rows,img_cols = 48,48 
+batch_size = 8 # how many models train at a time since my RAM is low i go with 8 no
 
 train_data_dir = r'H:\6th sem\project\images\train' #r for forward slash python not accept /
-validation_data_dir = r'H:\6th sem\project\images\validation'
+validation_data_dir = r'H:\6th sem\project\images\validation' 
 
 train_datagen = ImageDataGenerator(
 					rescale=1./255,
@@ -24,7 +24,7 @@ train_datagen = ImageDataGenerator(
 					fill_mode='nearest')
                     #5 to 6 image will create from 1 image,hor_flip=mirror photo
 
-validation_datagen = ImageDataGenerator(rescale=1./255)
+validation_datagen = ImageDataGenerator(rescale=1./255) #it is used for crosscheck the image
 
 train_generator = train_datagen.flow_from_directory(
 					train_data_dir,
@@ -42,19 +42,20 @@ validation_generator = validation_datagen.flow_from_directory(
 							batch_size=batch_size,
 							class_mode='categorical',
 							shuffle=True)
+                            #target color mode=greyscale image
 
 
-model = Sequential()
+model = Sequential() #simple model
 
 # Block-1  ( 3,3 size taken and  stread = 2,2 padding means = how many pixels taking 
 
-model.add(Conv2D(32,(3,3),padding='same',kernel_initializer='he_normal',input_shape=(img_rows,img_cols,1)))#1 for grey scale
-model.add(Activation('elu'))
+model.add(Conv2D(32,(3,3),padding='same',kernel_initializer='he_normal',input_shape=(img_rows,img_cols,1)))#1 for grey scale ,padding amount of pixel added to an image when (32 neurons)
+model.add(Activation('elu'))# x x>e0 and alpha(e^x-1) x<0
 model.add(BatchNormalization()) #nerual network
 model.add(Conv2D(32,(3,3),padding='same',kernel_initializer='he_normal',input_shape=(img_rows,img_cols,1)))
-model.add(Activation('elu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Activation('elu'))#cross the thersold value o/p then only it go for output
+model.add(BatchNormalization())#it is used to speed up the training neral network(performance and stubilty)
+model.add(MaxPooling2D(pool_size=(2,2)))#2x2
 model.add(Dropout(0.2))
 
 # Block-2 
@@ -108,7 +109,7 @@ model.add(Dropout(0.5))
 # Block-7
 
 model.add(Dense(num_classes,kernel_initializer='he_normal'))
-model.add(Activation('softmax')) #suppose 2 output
+model.add(Activation('softmax')) #used to get the output from 5 sample/cat
 
 print(model.summary())
 
@@ -119,7 +120,7 @@ checkpoint = ModelCheckpoint('Emotion_little_vgg.h5',
                              monitor='val_loss',
                              mode='min',
                              save_best_only=True,
-                             verbose=1)
+                             verbose=1)#if model accuracy is not improving , vgg.h5 is generated one
 
 earlystop = EarlyStopping(monitor='val_loss',
                           min_delta=0,
@@ -127,22 +128,22 @@ earlystop = EarlyStopping(monitor='val_loss',
                           verbose=1,
                           restore_best_weights=True
                           )
-                          #used to stop the traning
+                          #used to stop the traning if accuracy is not best one
 
 reduce_lr = ReduceLROnPlateau(monitor='val_loss',
                               factor=0.2,
                               patience=3,
                               verbose=1,
-                              min_delta=0.0001)
+                              min_delta=0.0001) #reduce learning rate slow down the learning .
 
-callbacks = [earlystop,checkpoint,reduce_lr]
+callbacks = [earlystop,checkpoint,reduce_lr] #callback lst
 
 model.compile(loss='categorical_crossentropy',
               optimizer = Adam(lr=0.001),
-              metrics=['accuracy']) #compiler part
+              metrics=['accuracy']) #compiler part , adam is optimizer
 
-nb_train_samples = 24176
-nb_validation_samples = 3006
+nb_train_samples = 24176 #no of training samples 7000*5
+nb_validation_samples = 3006 #no of validation samples
 epochs=25  # 25 times it train the program
 #fit for traning purpose
 history=model.fit_generator( 
@@ -151,4 +152,4 @@ history=model.fit_generator(
                 epochs=epochs,
                 callbacks=callbacks,
                 validation_data=validation_generator,
-                validation_steps=nb_validation_samples//batch_size)
+                validation_steps=nb_validation_samples//batch_size) #fitgen is start to generate the training part
